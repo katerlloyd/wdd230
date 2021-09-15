d92d373441337e58eabf5bfc092adb885fe28403
katereclark <cla16022@byui.edu>
2021-09-14 21:40:43 -0600

    Modified date and colors

--- js/index.js
+++ js/index.js
@@ -1,22 +1,7 @@
-let monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-
 let date = new Date;
-let currentMonthName = monthNames[date.getMonth()];
+
 let currentYear = date.getFullYear();
-
-let fullDate = currentMonthName + " " + date.getDate() + ", " + currentYear;
-
 document.getElementById("currentYear").textContent = currentYear;
+
+let dateLastModified = document.lastModified;
+document.getElementById("dateLastUpdated").textContent = dateLastModified;
