5a3cf96c9114f0880996d2c0a47c026cc08e725c
katereclark <cla16022@byui.edu>
2021-09-20 20:36:20 -0600

    Set up lesson2 folders

---
+++ lesson2/js/dates.js
@@ -0,0 +1,3 @@
+document.querySelector("#lesson2-copyright-year").textContent = new Date().getFullYear();
+
+document.getElementById("lesson2-date-last-updated").textContent = document.lastModified;
