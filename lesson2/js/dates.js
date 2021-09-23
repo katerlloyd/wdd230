87b71186ea08edf353e6244b08870410ebb12eb2
katereclark <cla16022@byui.edu>
2021-09-22 21:56:32 -0600

    Fixed minor errors

--- lesson2/js/dates.js
+++
@@ -1,3 +0,0 @@
-document.querySelector("#lesson2-copyright-year").textContent = new Date().getFullYear();
-
-document.getElementById("lesson2-date-last-updated").textContent = document.lastModified;
