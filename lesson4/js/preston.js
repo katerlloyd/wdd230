8218b0b857706342466d0fcc8239d87cdffcb59b
katereclark <cla16022@byui.edu>
2021-10-07 13:28:18 -0600

    Fixed error

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -8,7 +8,7 @@ window.onresize = () => {
 
 const date = new Date(Date.now());
 
-document.querySelector("#preston-copyright-year").textContent = date.getFullYear();
-
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.querySelector("#current-date").textContent = date.toLocaleDateString('en-UK', options);
+
+document.querySelector("#preston-copyright-year").textContent = date.getFullYear();
