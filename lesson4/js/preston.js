762ff5f8f8e35851c6a715bf64b9520b2ab1f986
katereclark <cla16022@byui.edu>
2021-10-06 22:03:10 -0600

    Added main padding

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -6,35 +6,7 @@ window.onresize = () => {
     if (window.innerWidth > 675) document.querySelector('.navigation').classList.remove('responsive')
 };
 
-let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-
-let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
-
 const date = new Date(Date.now());
 
-document.getElementById("current-date").textContent = days[date.getDay()] + ", " + date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
-
-
-// const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-// document.querySelector("#current-date").textContent = date.toLocaleDateString(undefined, options);
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.querySelector("#current-date").textContent = date.toLocaleDateString('en-UK', options);
