85d0ee95646ab5032ae1bbd8de0988408e57c086
katereclark <cla16022@byui.edu>
2021-10-11 15:43:24 -0600

    Added a favicon

---
+++ lesson5/js/preston.js
@@ -0,0 +1,14 @@
+document.querySelector('.bar-icon').addEventListener('click', () => {
+    document.querySelector('.navigation').classList.toggle('responsive');
+}, false);
+
+window.onresize = () => {
+    if (window.innerWidth > 675) document.querySelector('.navigation').classList.remove('responsive')
+};
+
+const date = new Date(Date.now());
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.querySelector("#current-date").textContent = date.toLocaleDateString('en-UK', options);
+
+document.querySelector("#preston-copyright-year").textContent = date.getFullYear();
