ee4a9a3b41c77f31ceafcb3d5e1e4b08d2c5b00f
katereclark <cla16022@byui.edu>
2021-10-12 12:34:37 -0600

    Added banner code

--- dom-manipulation/js/bom.js
+++ dom-manipulation/js/bom.js
@@ -1,4 +1,3 @@
-
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const list = document.querySelector('.list');
--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -12,3 +12,12 @@ const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'
 document.querySelector("#current-date").textContent = date.toLocaleDateString('en-UK', options);
 
 document.querySelector("#preston-copyright-year").textContent = date.getFullYear();
+
+if (date.getDay() === 5) {
+    let banner = document.createElement('div');
+    banner.classList.add('friday-banner');
+    banner.textContent = 'Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.';
+
+    const main = document.querySelector('main');
+    main.prepend(banner);
+}
