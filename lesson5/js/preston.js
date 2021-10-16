1d6e408a2417d1d26cdbbc70202faa9fbfe51d30
katereclark <cla16022@byui.edu>
2021-10-16 13:02:12 -0600

    Added weather summary and hero image

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -13,12 +13,12 @@ document.querySelector("#current-date").textContent = date.toLocaleDateString('e
 
 document.querySelector("#preston-copyright-year").textContent = date.getFullYear();
 
-if (date.getDay() === 3) {
+if (date.getDay() === 5) {
     let banner = document.createElement('div');
     banner.classList.add('friday-banner');
-    banner.textContent = '🥞 Saturday | Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.';
+    banner.textContent = '🥞 Saturday | Preston Pancakes in the Park!  9:00 AM Saturday at the city park pavilion.';
 
-    const main = document.querySelector('header');
+    const main = document.querySelector('body');
     main.prepend(banner);
 } else {
     bannerClass = document.querySelector('friday-banner');
