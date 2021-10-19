f5624536380ebc9eb917064161027b6dc5bb5939
katereclark <cla16022@byui.edu>
2021-10-18 20:26:18 -0600

    Added web font loader

---
+++ lesson6/js/preston.js
@@ -0,0 +1,28 @@
+document.querySelector('.bar-icon').addEventListener('click', () => {
+    document.querySelector('.navigation').classList.toggle('responsive');
+}, false);
+
+window.onresize = () => {
+    if (window.innerWidth > 675) document.querySelector('.navigation').classList.remove('responsive')
+};
+
+const date = new Date(Date.now());
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.querySelector("#current-date").textContent = date.toLocaleDateString('en-UK', options);
+
+document.querySelector("#preston-copyright-year").textContent = date.getFullYear();
+
+if (date.getDay() === 5) {
+    let banner = document.createElement('div');
+    banner.classList.add('friday-banner');
+    banner.textContent = '🥞 Saturday | Preston Pancakes in the Park! &nbsp;9:00 AM Saturday at the city park pavilion.';
+
+    const main = document.querySelector('body');
+    main.prepend(banner);
+} else {
+    bannerClass = document.querySelector('friday-banner');
+    if (bannerClass != null) {
+        banner.remove('friday-banner');
+    }
+}
