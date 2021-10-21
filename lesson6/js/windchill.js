b1cc2500447539cf2cb2017144f3b4d9fd894cd5
katereclark <cla16022@byui.edu>
2021-10-20 20:33:40 -0600

    Fixed windchill formula

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,7 +3,7 @@ let s = document.querySelector(".windspeed").textContent;
 let windchill_factor = 'N/A';
 
 if ((t <= 50) && (s > 3)) {
-    windchill_factor = Math.round(35.74 + (0.6215 * t ) - (35.75 * s * 0.16) + (0.4275 * t * s * 0.16));
+    windchill_factor = Math.round(35.74 + (0.6215 * t ) - (35.75 * Math.pow(s, 0.16)) + ((0.4275 * t) * Math.pow(s, 0.16)));
 } 
 
 document.querySelector(".windchill").textContent = windchill_factor;
