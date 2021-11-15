68a33ca234667748a246a021dff168f9382c33e5
katereclark <cla16022@byui.edu>
2021-11-15 16:33:50 -0700

    Added weather API

---
+++ lesson10/js/index.js
@@ -0,0 +1,79 @@
+//#region Responsive Navigation Bar
+document.querySelector('.bar-icon').addEventListener('click', () => {
+    document.querySelector('.navigation').classList.toggle('responsive');
+}, false);
+
+window.onresize = () => {
+    if (window.innerWidth > 675) document.querySelector('.navigation').classList.remove('responsive')
+};
+//#endregion 
+
+
+//#region Copyright Year and Current Date
+const date = new Date(Date.now());
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.querySelector("#home-current-date").textContent = date.toLocaleDateString('en-UK', options);
+
+document.querySelector("#home-copyright-year").textContent = date.getFullYear();
+//#endregion 
+
+//#region API Request
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL).then(function (response) {return response.json();}).then(function (jsonObject) {
+    console.table(jsonObject);
+    const towns = jsonObject['towns'];
+    for (let i = 0; i < towns.length; i++ ) {
+        if (towns[i].name === "Preston" || towns[i].name === "Fish Haven" || towns[i].name === "Soda Springs") {
+    
+            let towncontainer = document.createElement('div');
+            towncontainer.classList.add('town-container');
+            let town = document.createElement('section');
+            town.classList.add('town-data');
+            let h2 = document.createElement('h2');
+            let a = document.createElement('a');
+            let q = document.createElement('q');
+            let paragraphs = document.createElement('section');
+            paragraphs.classList.add('paragraph-block');
+            let p1 = document.createElement('p');
+            let p2 = document.createElement('p');
+            let p3 = document.createElement('p');
+            let imagesection = document.createElement('section');
+            imagesection.classList.add('town-img');
+            let img = document.createElement('img');
+
+            if (towns[i].name === "Preston") {
+                a.setAttribute('href', "preston-9.html");
+            } else if (towns[i].name === "Fish Haven") {
+                a.setAttribute('href', "#");
+            } else if (towns[i].name === "Soda Springs") {
+                a.setAttribute('href', "#");
+            } else {
+                a.setAttribute('href', "#");
+            }
+
+            a.textContent = towns[i].name;
+            q.textContent = towns[i].motto;
+            p1.textContent = 'Year Founded: ' + towns[i].yearFounded;
+            p2.textContent = 'Population: ' + towns[i].currentPopulation;
+            p3.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
+            img.setAttribute('src', 'images/' + towns[i].photo);
+            img.setAttribute('alt', towns[i].name);
+
+            h2.appendChild(a);
+            town.appendChild(h2);
+            town.appendChild(q);
+            paragraphs.appendChild(p1);
+            paragraphs.appendChild(p2);
+            paragraphs.appendChild(p3);
+            town.appendChild(paragraphs);
+            imagesection.appendChild(img);
+            towncontainer.appendChild(town);
+            towncontainer.appendChild(imagesection);
+
+            document.querySelector('div.home-grid').appendChild(towncontainer);
+        }
+    }
+});
+//#endregion
---
+++ lesson10/lesson-10/js/weatherapi.js
@@ -0,0 +1,14 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=c3affef608fbd43350f108a8f72cddac&units=imperial';
+
+fetch(apiURL).then((response) => response.json()).then((jsonObject) => {
+    console.log(jsonObject);
+    
+    const current_temp = document.querySelector('current-temp');
+    current_temp.textContent = jsonObject.main.temp;
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsonObject.weather[0].icon + '.png';
+    const desc = jsonObject.weather[0].description;  
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
