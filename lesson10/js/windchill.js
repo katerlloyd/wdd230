0cd51b9e9ebbfb6e57fa080a9d320757eb74793a
katereclark <cla16022@byui.edu>
2021-11-16 16:00:04 -0700

    Fix windchill formula

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -5,20 +5,23 @@ fetch(apiURL).then((response) => response.json()).then((jsonObject) => {
     
     const temperature = document.querySelector('.temperature');
     temperature.textContent = jsonObject.main.temp;
+    t = jsonObject.main.temp;
 
     const currently = document.querySelector('.currently');
     currently.textContent = jsonObject.weather[0].description;
 
     const windspeed = document.querySelector('.windspeed');
     windspeed.textContent = jsonObject.wind.speed;
+    w = jsonObject.wind.speed;
+
 
     const humidity = document.querySelector('.humidity');
     humidity.textContent = jsonObject.main.humidity;
 
     let windchill_factor = 'N/A';
 
-    if ((temperature <= 50) && (windspeed > 3)) {
-        windchill_factor = Math.round(35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, 0.16)) + ((0.4275 * temperature) * Math.pow(windspeed, 0.16))) + '\&#176;F';
+    if ((t <= 50) && (w > 3)) {
+        windchill_factor = Math.round(35.74 + (0.6215 * t) - (35.75 * Math.pow(w, 0.16)) + ((0.4275 * t) * Math.pow(w, 0.16))) + '\&#176;F';
     } 
     
     document.querySelector('.windchill').textContent = windchill_factor;
