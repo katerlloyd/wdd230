ba49ba4dac80cfd393939d9aab9486c2d42f6534
katereclark <cla16022@byui.edu>
2021-11-26 11:28:05 -0700

    Added placeholders and lazy loading

--- lesson11/js/site.js
+++ lesson11/js/site.js
@@ -8,6 +8,39 @@ window.onresize = () => {
 };
 //#endregion
 
+//#region Lazy Loading
+const images = document.querySelectorAll('img[data-src]');
+
+const imageOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px 50px 0px'
+};
+
+function loadImages(image) {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {(image.removeAttribute('data-src'))};
+}
+
+if('IntersectionObserver' in window) {
+    const imageObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imageOptions);
+
+    images.forEach((img) => { 
+        imageObserver.observe(img); 
+    });
+} else {
+    images.forEach((img) => { 
+        loadImages(img); 
+    });
+}
+//#endregion 
+
 //#region Copyright Year and Current Date
 const date = new Date();
 
