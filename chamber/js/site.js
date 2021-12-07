862265ac9d02d3a4e2d581880e1d6ef6773b3b14
katereclark <cla16022@byui.edu>
2021-12-06 19:31:21 -0700

    Added grids to sections

--- chamber/js/site.js
+++ chamber/js/site.js
@@ -8,39 +8,6 @@ window.onresize = () => {
 };
 //#endregion
 
-//#region Lazy Loading
-const images = document.querySelectorAll('img[data-src]');
-
-const imageOptions = {
-    threshold: 0,
-    rootMargin: '0px 0px 50px 0px'
-};
-
-function loadImages(image) {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {(image.removeAttribute('data-src'))};
-}
-
-if('IntersectionObserver' in window) {
-    const imageObserver = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if(item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
-    }, imageOptions);
-
-    images.forEach((img) => { 
-        imageObserver.observe(img); 
-    });
-} else {
-    images.forEach((img) => { 
-        loadImages(img); 
-    });
-}
-//#endregion 
-
 //#region Copyright Year and Last Modified Date
 const date = new Date();
 
